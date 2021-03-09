4aac08ec4e3eb813a246176aded5ff8f976a576c
Vincent D'Alto <66323103+Vincent-glitch@users.noreply.github.com>
2021-03-09 15:36:27 -0500

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,35 +1,11 @@
-// Data.js information in variable
 var tableData = data;
-// 
-var tbody = d3.select("tbody");
 
-// forEach on the TableData
-tableData.forEach((sighting) => {
-    var row = tbody.append("tr");
-    Object.entries(sighting).forEach(([key, value]) => {
-      var cell = row.append("td");
-      cell.text(value);
+var tbody = d3.select("tbody");
+tableData.forEach(function(dataSearch) {
+	    var row = tbody.append("tr");
+	Object.entries(dataSearch).forEach(function([key, value]) {
+		var cell = row.append("td");
+		cell.text(value);
     });
   });
-
-var button = d3.select("#filter-btn");
-var form =  d3.select("#form");
-//Couldn't enter to work the same as clicking to run enter
-button.on("click", runEnter);
-
-    function runEnter(){
-        d3.event.preventDefault();
-        tbody.html("");
-        var inputElement = d3.select("#datetime");
-        var inputValue = inputElement.property("value");
-        var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
-
-    filteredData.forEach((sighting) => {
-        var row = tbody.append("tr");
-        Object.entries(sighting).forEach(([key, value]) => {
-            var cell = row.append("td");
-            cell.text(value);
-        });
-    });
-
-    };
+  
